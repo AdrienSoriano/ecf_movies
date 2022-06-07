f165ed5ff596c56940e7e1e4842922512239a434
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-06-07 11:03:14 +0200

    Add
    Adding the request file showing the movies title.

---
+++ requests/01-getTitleMovies.php
@@ -0,0 +1,11 @@
+<?php
+
+// Réaliser une requête qui permet de récupérer tous les titres des films
+include ('../connect.php');
+
+
+$get_movies_title = "SELECT title FROM infos_movies";
+$req_movie = $db->query($get_movies_title);
+    while($res=$req_movie->fetch()){
+        print_r($res['title']."\n");
+    }
