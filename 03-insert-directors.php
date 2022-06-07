c48842f9ee4c8792798b2d34ffdf79ce0c90962f
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-06-07 10:28:26 +0200

    Up
    Modification of the SQL requests.

--- 03-insert-directors.php
+++ 03-insert-directors.php
@@ -2,7 +2,7 @@
 
 require_once('connect.php');
 
-$req_insert_directors = "INSERT INTO directors (director_name) SELECT * FROM (SELECT :name_director AS director_name) AS temp WHERE NOT EXISTS ( SELECT director_name FROM directors WHERE director_name = :name_director )";
+$req_insert_directors = "INSERT INTO directors (`director_name`) SELECT * FROM (SELECT :name_director AS `director_name`) AS temp WHERE NOT EXISTS ( SELECT `director_name` FROM directors WHERE `director_name` = :name_director )";
 
 $req_dir = $db->prepare($req_insert_directors);
 
