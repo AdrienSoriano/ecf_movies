0863e8900b448a31e59498ce6eeca7429aa1d7d5
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-06-07 09:28:42 +0200

    Up
    Modified variable for the prepare function.

--- 03-insert-directors.php
+++ 03-insert-directors.php
@@ -4,7 +4,7 @@ require_once('connect.php');
 
 $req_insert_directors = "INSERT INTO directors (director_name) SELECT * FROM (SELECT :name_director AS director_name) AS temp WHERE NOT EXISTS ( SELECT director_name FROM directors WHERE director_name = :name_director )";
 
-$req_dir = $conn->prepare($req_insert_directors);
+$req_dir = $db->prepare($req_insert_directors);
 
 $file = fopen('./film.csv','r');
 if ($file !==FALSE) {
